19bced5458c5a5a97f701a78434852ace417b14e
axelauza97 <axelauza31@gmail.com>
2024-06-24 02:19:46 -0500

    Add new page for confirmation

--- src/components/TagControl.jsx
+++ src/components/TagControl.jsx
@@ -11,17 +11,7 @@ import {
   Select,
 } from "@mui/material";
 import { Controller, useFormContext } from "react-hook-form";
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-const tags = ["UI", "Backend", "Performance"];
+import { MenuProps, tags } from "../utils/Constants";
 
 export const TagControl = ({ error, helperText }) => {
   const { control } = useFormContext();
